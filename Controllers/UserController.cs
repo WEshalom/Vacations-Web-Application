052f9ee5dad139a8439a53993d8aee34c35e6210
Shalom Weiss <shalomweiss07@gmail.com>
2017-04-13 22:18:04 -0400

    vacations project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VacationsProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+    }
+}
